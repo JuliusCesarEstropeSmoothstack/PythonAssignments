5cde658f2df7e12e0333854e866450012d4df119
Julius Cesar Estrope <julius.estrope@smoothstack.com>
2021-11-10 11:34:30 -0500

    Parts 1-4 of assignment 2

---
+++ Assignment2/part1.py
@@ -0,0 +1,5 @@
+total = 50 + 50
+diff = 100 - 10
+
+print(sum)
+print(diff)
---
+++ Assignment2/part4.py
@@ -0,0 +1,17 @@
+P = 800000
+R = 6
+L = 103
+tentativeM = 10000
+remainder = 0
+
+for x in range(L):
+    P = P * R / 100 / 12 - tentativeM
+
+if P > 0:
+    remainder = P
+
+totalPayment = (L - 1) * tentativeM + remainder
+
+M = totalPayment / L
+
+print(int(M))
